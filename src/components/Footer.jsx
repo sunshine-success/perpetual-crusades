3ef9ad49ff72d90e33defc985563f93cf421525b
sunshine <tylor.arpoika.pro@gmail.com>
2023-10-09 14:29:21 +0200

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div class="footer">
+      <h5>
+        Created with{" "}
+        <span role="img" aria-label="heart">
+          ❤️
+        </span>{" "}
+        by{" "}
+        <a style={{ color: "white" }} href="https://www.adityavsingh.com/">
+          Aditya Vikram Singh
+        </a>
+        , 2020
+      </h5>
+    </div>
+  );
+};
+
+export default Footer;
